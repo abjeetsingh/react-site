0f6b71acdd93c96d13aa0dcd724e4ec5cb871412
Abjeet Singh <abjeets50@gmail.com>
2023-08-12 10:27:52 +0530

    feat: create app component

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Hero(){
+  return (
+    <section className="hero">
+      <img className="hero-photo" src="/src/assets/photo-grid.png"></img>
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import ReactDOM from "react/client"
+
+function Navbar(){
+  return (
+    <nav className = "navbar">
+        <img className="airlogo" src="src/assets/airbnb-logo.png"></img>
+      </nav>
+  )
+}
+
+export default Navbar
