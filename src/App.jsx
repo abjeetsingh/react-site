77949069dfb40628754825adbf8573aa21204849
Abjeet Singh <abjeets50@gmail.com>
2023-08-14 06:12:40 +0530

    refactor: auto update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import './App.css'
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
 import Card from "./Components/Card.jsx"
+import data from  "./assets/data.js"
 function App() {
   
+  const Cards = data.map( item => {return( 
+    <Card 
+        img={ "/assets/"  + item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+   )} )
   return (
     <>
       <Navbar />
       {/* <Hero /> */}
-      <Card />
+      <section className="card-list">
+      {Cards}
+      </section>
     </>
   )
 }
